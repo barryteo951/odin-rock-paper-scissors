0ae74d05074f2eedae29a658e4cf360d47938423
bar66 <101086153+barryteo951@users.noreply.github.com>
2026-03-23 11:58:39 +1100

    feat: first to 5 point wins

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 // REQUIREMENTS
-// For now, remove the logic that plays exactly five rounds. [DONE]
-// Create three buttons, one for each selection. [DONE]
-    // Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step) [DONE]
-// Add a div for displaying results and change all of your console.logs into DOM methods. [DONE]
-// Display the running score, and announce a winner of the game once one player reaches 5 points.
+// [OK] For now, remove the logic that plays exactly five rounds. 
+// [OK] Create three buttons, one for each selection. [DONE]
+    // Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step) 
+// [OK] Add a div for displaying results and change all of your console.logs into DOM methods. 
+// [OK] Display the running score, and announce a winner of the game once one player reaches 5 points. 
 // You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
-
-// 
-
-// BRAINSTORMING
-// 1. When user clicks button, that's a human choice. 
-// 2. The computer makes choice, that's a computer choice. 
-// 3. The winner is decided, and score is updated. 
 
 
 
 const buttons = document.querySelectorAll("button");
-// const buttonRock = document.querySelector(".buttons .rock");
-// const buttonPaper = document.querySelector(".buttons .paper");
-// const buttonScissors = document.querySelector(".buttons .scissors");
 const divMessage = document.querySelector(".message")
 const divHumanScore = document.querySelector(".scores .human");
 const divComputerScore = document.querySelector(".scores .computer"); 
-
-
 
 playGame();
 
@@ -50,58 +38,20 @@ function getComputerChoice() {
 
 
 
-// function getHumanChoice() {
-//     let humanChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
-
-//     switch (humanChoice) {
-//         case "rock": 
-//             humanChoice = "rock";
-//             break;
-//         case "paper":
-//             humanChoice = "paper";
-//             break;
-//         case "scissors":
-//             humanChoice = "scissors";
-//             break;
-//         default: 
-//             alert("Invalid prompt");
-//             break;
-//     }
-
-//     return humanChoice;
-// }
-
-
-
 function playGame() {
     let humanScore = 0; 
     let computerScore = 0; 
+    let humanChoice = "";
+    let computerChoice = "";
 
-    // while (humanScore < 5 && computerScore < 5) {
-    // let humanChoice = ""getHumanChoice""();
-        let humanChoice = "";
-        let computerChoice = "";
-        buttons.forEach((button) => {
-            button.addEventListener("click", () => {
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
             humanChoice = button.className;
             playRound(humanChoice);
-            });
-        }); 
-    // buttonRock.addEventListener("click", (event) => {
-    //     humanChoice = buttonRock.className;
-    //     playRound(humanChoice);
-    // }); 
-    // buttonPaper.addEventListener("click", (event) => {
-    //     humanChoice = buttonPaper.className;
-    //     playRound(humanChoice);
-    // });
-    // buttonScissors.addEventListener("click", (event) => {
-    //     humanChoice = buttonScissors.className;
-    //     playRound(humanChoice);
-    // });
-    // }
-
+        });
+    }); 
     
+
 
     function playRound(humanChoice) {
         computerChoice = getComputerChoice();
@@ -109,14 +59,28 @@ function playGame() {
         if (humanChoice == computerChoice) {
             divMessage.textContent = "Draw!";
         } else if ((humanChoice=="rock" && computerChoice=="scissors") || (humanChoice=="scissors" && computerChoice=="paper") || (humanChoice=="paper" && computerChoice=="rock")) {           
-            divMessage.textContent = "You win!";
+            divMessage.textContent = "Win!";
             humanScore++;
         } else {
-            divMessage.textContent = "You lose!";
+            divMessage.textContent = "Lose!";
             computerScore++;
         }
 
         divHumanScore.textContent = humanScore;
         divComputerScore.textContent = computerScore;
+
+        checkWinner(humanScore, computerScore);
+        }
     }
-}
+
+
+function checkWinner(humanScore, computerScore) {
+    if (humanScore >= 5) { 
+        divMessage.textContent = "YOU WIN, CONGRATS!";
+        return;
+    }
+    else if (computerScore >= 5) {
+        divMessage.textContent = "YOU LOSE. TRY AGAIN..";
+        return;
+    }
+}  
